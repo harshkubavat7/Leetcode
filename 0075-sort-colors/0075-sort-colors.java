f26728d1aaceafcc52dae2c0d9a959b0d21f1b22
Harsh Kubavat <hkubavat15@gmail.com>
2026-06-04 18:22:00 +0530

    Time: 0 ms (100%), Space: 43.5 MB (40.51%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,23 +1,54 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int temp;
-        boolean swap;
-        for(int i=0;i<nums.length;i++)
-        {
-            swap=false;
-            for(int j=0;j<nums.length-i-1;j++)//after first short largest is at last to reduce step n-1
-            {
-                if(nums[j]>nums[j+1])
-                {
-                    temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-                    swap=true;
-                }
-            }
-            if(!swap)//if already sorted then arter one loop it breaks
-            {
-                break;
-            }
+    //     int temp;
+    //     boolean swap;
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         swap=false;
+    //         for(int j=0;j<nums.length-i-1;j++)//after first short largest is at last to reduce step n-1
+    //         {
+    //             if(nums[j]>nums[j+1])
+    //             {
+    //                 temp=nums[j];
+    //                 nums[j]=nums[j+1];
+    //                 nums[j+1]=temp;
+    //                 swap=true;
+    //             }
+    //         }
+    //         if(!swap)//if already sorted then arter one loop it breaks
+    //         {
+    //             break;
+    //         }
+    // }
+
+    // Arrays.sort(nums);
+
+
+    int start = 0;
+    int mid = 0;
+    int end = nums.length-1;
+
+    while(mid<=end){
+        if(nums[mid]==0){
+            swap(nums,start,mid);
+            mid++;
+            start++;
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums,mid,end);
+            
+            end--;
+        }
     }
-}}
+}
+
+    public static void swap(int[] nums,int num1,int num2){
+        int temp;
+        temp=nums[num1];
+        nums[num1]=nums[num2];
+        nums[num2]=temp;
+    }
+}
