d3ef63e21c071aabbe3d9bcc1047f6fb58aeea99
Harsh Kubavat <hkubavat15@gmail.com>
2026-06-05 10:03:45 +0530

    Time: 20 ms (99.26%), Space: 77.2 MB (96.84%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,33 +1,56 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int n = nums.length;
-        
-        HashSet<Integer> set = new HashSet<>();
-
-        for(int i=0;i<n;i++){
-            set.add(nums[i]);
-        }
-
-        int longest =0;
-        
-        for(int num : set){
-            if(!set.contains(num-1)){
-                int current = num;
-                int count=1;
-                while(set.contains(current+1)){
-                    current++;
-                    count++;
-                }
-
+        if(nums.length == 0) return 0;
+        Arrays.sort(nums);
+        int count=1;
+        int longest=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] == nums[i - 1]) {
+                continue; // ignore duplicates
+            }
+            else if(nums[i]-nums[i-1]==1){
+                count++;
                 longest = Math.max(count,longest);
-
-
-
+            }
+            else{
+                count=1;
             }
         }
 
-        
         return longest;
-
     }
 }
+
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         int n = nums.length;
+        
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for(int i=0;i<n;i++){
+//             set.add(nums[i]);
+//         }
+
+//         int longest =0;
+        
+//         for(int num : set){
+//             if(!set.contains(num-1)){
+//                 int current = num;
+//                 int count=1;
+//                 while(set.contains(current+1)){
+//                     current++;
+//                     count++;
+//                 }
+
+//                 longest = Math.max(count,longest);
+
+
+
+//             }
+//         }
+
+        
+//         return longest;
+
+//     }
+// }
