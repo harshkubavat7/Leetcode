ddea18af84c1622c1b6806381bffefcf9fc10e37
Harsh Kubavat <hkubavat15@gmail.com>
2026-06-05 16:10:55 +0530

    Time: 14 ms (41.83%), Space: 53.3 MB (7.52%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,53 +1,71 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n=nums.length;
-        // if(n<2)
+        // int n=nums.length;
+        // // if(n<2)
+        // // {
+        // //     return nums;
+        // // }
+        // int first=nums[0];
+        // int second=nums[0];
+        // int fc=0;
+        // int sc=0;
+        // for(int i=0;i<n;i++)
         // {
-        //     return nums;
+        //     if(first==nums[i])
+        //     {
+        //         first=nums[i];
+        //         fc++;
+        //     }
+        //     else if(second==nums[i])
+        //     {
+        //         second=nums[i];
+        //         sc++;
+        //     }
+        //     else if(fc==0)
+        //     {
+        //         first=nums[i];
+        //         fc++;
+        //     }
+        //     else if(sc==0)
+        //     {
+        //         second=nums[i];
+        //         sc++;
+        //     }
+        //     else{
+        //         fc--;
+        //         sc--;
+        //     }
         // }
-        int first=nums[0];
-        int second=nums[0];
-        int fc=0;
-        int sc=0;
-        for(int i=0;i<n;i++)
-        {
-            if(first==nums[i])
-            {
-                first=nums[i];
-                fc++;
-            }
-            else if(second==nums[i])
-            {
-                second=nums[i];
-                sc++;
-            }
-            else if(fc==0)
-            {
-                first=nums[i];
-                fc++;
-            }
-            else if(sc==0)
-            {
-                second=nums[i];
-                sc++;
-            }
-            else{
-                fc--;
-                sc--;
+        // // if(first==second) return first;
+        // fc=sc=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(first==nums[i]) fc++;
+        //     else if(second==nums[i]) sc++;
+
+        // }
+        // List<Integer> ans = new ArrayList<>();
+        // if(fc>n/3) ans.add(first);
+        // if(sc>n/3) ans.add(second);
+
+        // return ans;
+
+        List<Integer> list = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        for(int num : map.keySet()){
+            if(map.get(num)>(nums.length/3)){
+                list.add(num);
             }
         }
-        // if(first==second) return first;
-        fc=sc=0;
-        for(int i=0;i<n;i++)
-        {
-            if(first==nums[i]) fc++;
-            else if(second==nums[i]) sc++;
 
-        }
-        List<Integer> ans = new ArrayList<>();
-        if(fc>n/3) ans.add(first);
-        if(sc>n/3) ans.add(second);
+        return list;
 
-        return ans;
 }
+
+
 }
