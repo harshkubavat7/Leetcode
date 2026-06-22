bf693f9666bff58b572da60fcf8a710f299f9b88
Harsh Kubavat <hkubavat15@gmail.com>
2026-06-23 00:18:54 +0530

    Time: 5 ms (85.94%), Space: 44.6 MB (38.51%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,36 +1,56 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder res = new StringBuilder();
-        int startIndex = s.length()-1;
-        while(startIndex>=0){
-            while(startIndex>=0 && s.charAt(startIndex)==' ')
-        {
-            startIndex--;
-        }
-        //if no word available
-        if(startIndex<0)
-        {
-            break;
-        }
-        //one whole sentence since last space
-        int endIndex = startIndex;
-        while(startIndex>=0 && s.charAt(startIndex)!=' ' )
-        {
-            startIndex--;
-        }
-        //after getting sentence
-        if(res.length()==0)
-        {
-            res.append(s.substring(startIndex+1,endIndex+1));
+        String[] arr = s.trim().split(" ");
+        StringBuilder sb = new StringBuilder();
 
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i].length()>0){
+                sb.append(arr[i]);
+                sb.append(" ");
+            }
         }
-        else{
-            res.append(" ");
-            res.append(s.substring(startIndex+1,endIndex+1));
-        }
-        
-    }
-        return res.toString();
+        return sb.substring(0,sb.length()-1).toString();
     }
         
 }
+
+
+
+
+// //Start from the end of the string to reverse word order.
+//         //res will store the final result
+//         StringBuilder res = new StringBuilder();
+//         int startIndex = s.length()-1;
+
+//         //Skip trailing or extra spaces at the end or between words.
+//         while(startIndex>=0){
+//             //Move backward until we reach the beginning of the word.
+//         while(startIndex>=0 && s.charAt(startIndex)==' ')
+//         {
+//             startIndex--;
+//         }
+//         //If all characters are spaces, break the loop.
+//         if(startIndex<0)
+//         {
+//             break;
+//         }
+//         //one whole sentence since last space
+//         //endIndex marks the end of the current word.
+//         int endIndex = startIndex;
+//         while(startIndex>=0 && s.charAt(startIndex)!=' ' )
+//         {
+//             startIndex--;
+//         }
+//         //after getting sentence
+//         if(res.length()==0)
+//         {
+//             res.append(s.substring(startIndex+1,endIndex+1));
+
+//         }
+//         else{
+//             res.append(" ");
+//             res.append(s.substring(startIndex+1,endIndex+1));
+//         }
+        
+//     }
+//         return res.toString();
